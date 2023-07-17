b3dcd6828c33f3208d8a4e943a9225a310c33364
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-07-17 17:52:19 +0600

    creae user post

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const todoHandler = require("./routesHandler/todoHandler");
+// const todoHandler = require("./routesHandler/todoHandler");
 
 // require dotenv
 require("dotenv").config();
@@ -16,17 +17,41 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wuwpwwx.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  const usersCollection = client.db("seo-tools").collection("users");
+  try {
+    // post user data in mongodb database
+    app.post("/users", async (req, res) => {
+      const users = req.body;
+      const result = await usersCollection.insertOne(users);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
 // database connection with mongoose
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wuwpwwx.mongodb.net/?retryWrites=true&w=majority`
-  )
-  .then(() => console.log("successfully connected to MongoDB"))
-  .catch((error) => console.log("Error connecting to MongoDb", error));
+// mongoose
+//   .connect(
+//     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wuwpwwx.mongodb.net/?retryWrites=true&w=majority`
+//   )
+//   .then(() => console.log("successfully connected to MongoDB"))
+//   .catch((error) => console.log("Error connecting to MongoDb", error));
 
 // application routes
 
-app.use("/todo", todoHandler);
+// app.use("/todo", todoHandler);
 // app.use("/", router);
 
 // default error handler
@@ -60,9 +85,4 @@ app.listen(port, () => {
 //   },
 // });
 
-async function run() {
-  try {
-  } finally {
-  }
-}
 run().catch(console.dir);
